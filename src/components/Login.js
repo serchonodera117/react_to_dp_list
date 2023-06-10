071c725e046efab43fdf0b0210b09cf7718641f5
serchonodera117 <sergio.lobo117@gmail.com>
2023-06-10 09:11:29 -0600

    modal register template

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import '../styles/app.css';
 import '../styles/login.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 function Login({onLogin}){
     const [savePass, setPassSaved] = useState(false);
@@ -92,10 +93,32 @@ function Login({onLogin}){
                     </form>
                     <hr></hr>
                     <div className="footer-btn">
-                        <button className='btn btn-success' onClick={startLogin}>Create Account</button>
+                        <button className='btn btn-success'data-bs-toggle="modal" 
+                        data-bs-target="#modal-login">
+                            Create Account
+                            </button>
                         <button className='btn btn-primary' onClick={startLogin}>Log in</button>
                     </div>
                 </div>
+            </div>
+
+
+            <div className="modal fade" id="modal-login" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body">
+                    ...
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
             </div>
         </div>
     );
